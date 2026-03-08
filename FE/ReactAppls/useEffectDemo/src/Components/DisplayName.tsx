71e398b8680a2238a76c6f36c146a3a6c707b31a
Anil Krishna <akumarkk@outlook.com>
2026-03-08 11:56:29 -0700

    when comp rerenders - nested comp rerendered

--- FE/ReactAppls/useEffectDemo/src/Components/DisplayName.tsx
+++ FE/ReactAppls/useEffectDemo/src/Components/DisplayName.tsx
@@ -1,10 +1,16 @@
 
+import "./DisplayName.css"
+
 var dnCount = 0;
 export const DisplayName = ({name}) => {
+    // let dnCount = 0;
+    dnCount++;
     return (
+        <div className="display-name">
         <p>
             dnCount : {dnCount}
             Name is {name};
         </p>
+        </div>
     )
 }
--- FE/ReactAppls/useEffectDemo/src/Components/DisplayNames.tsx
+++ FE/ReactAppls/useEffectDemo/src/Components/DisplayNames.tsx
@@ -1,17 +1,20 @@
 import { DisplayName } from "./DisplayName";
+import "./DisplayNames.css"
+
 var dnCount = 0;
 export const DisplayNames = () => {
     const names = ["Punith", "KGF yash"];
     dnCount++;
     
     return (
-        <>
+        <div className="display-names">
+            count : {dnCount}
         {names.map((n, index) => (
             <div key={index}>
-                count : {index}/{dnCount}
+                
                 <DisplayName name={n}></DisplayName>
             </div>
         ))}
-        </>
+        </div>
     )
 }
