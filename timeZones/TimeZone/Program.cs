f4b4b9b1a6eeaf422ca0d693c9a04727b7fb6d14
Anil Krishna <akumarkk@outlook.com>
2026-04-14 12:09:09 -0700

    timezone convert

---
+++ timeZones/TimeZone/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace  Program;
+class Program
+{
+    public static void Main()
+    {
+        DateTime dt = DateTime.Parse("2023-01-25T23:58:37");
+        DateTime utc = TimeZoneInfo.ConvertTimeToUtc(dt, TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time"));
+
+        Console.WriteLine($"dt {dt} utc {utc}");
+    }
+    
+}
